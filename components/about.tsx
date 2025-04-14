962011f5228fe6cb82f892e091b901055b60d1f0
Ankush Chhabra <ankushchhabra2002@gmail.com>
2025-04-15 00:41:44 +0530

    fix

--- components/about.tsx
+++ components/about.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import SectionHeading from "./section-heading";
 import { useSectionInView } from "@/lib/hooks";
 const About = () => {
-  const { ref } = useSectionInView("About", 0.7);
+  const { ref } = useSectionInView("About", 0.6);
 
   return (
     <motion.section
